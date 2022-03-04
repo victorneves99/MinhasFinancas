d3169debba9964af251d680c14d3f491034342d0
victorneves <victorneves99@gmail.com>
2022-03-04 19:02:59 -0300

    implementando regra de negocio

---
+++ src/main/java/com/victor/minhasfinancas/service/LancamentoService.java
@@ -0,0 +1,22 @@
+package com.victor.minhasfinancas.service;
+
+import java.util.List;
+
+import com.victor.minhasfinancas.model.entity.Lancamento;
+import com.victor.minhasfinancas.model.enums.StatusLancamento;
+
+public interface LancamentoService {
+
+    Lancamento salvar(Lancamento lancamento);
+
+    Lancamento atualizar(Lancamento lancamento);
+
+    void deletar(Lancamento lancamento);
+
+    List<Lancamento> buscar(Lancamento lancamentoFiltro);
+
+    void atualizarStatus(Lancamento lancamento, StatusLancamento status);
+
+    void validar(Lancamento lancamento);
+
+}
