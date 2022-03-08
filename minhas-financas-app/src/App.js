259291c0c545263160a15f5fa5e0ab29a210d92a
victorneves <victorneves99@gmail.com>
2022-03-08 19:48:18 -0300

    centralizando o componente Login na Pagina

--- minhas-financas-app/src/App.js
+++ minhas-financas-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "bootswatch/dist/lux/bootstrap.css";
 import Login from "./views/login";
+import './custom.css'
 export default class App extends Component {
   render() {
     return (
